5d660016f9b62ba88cd9d18867b9d494951f7158
Olivia Kim <oliviakim321@gmail.com>
2023-06-06 13:00:16 -0400

    add android bridge & unity sdk (#1)

---
+++ Attentive.Unity/Attentive.Unity/Android/AttentiveAndroidBridge.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using Attentive.Unity.Models;
+
+
+namespace Attentive.Unity.Android
+{
+    public class AttentiveAndroidBridge
+    {
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+
+        private AndroidJavaObject attentiveAndroidWrapper;
+        private AndroidJavaObject playerActivity;
+
+        public AttentiveAndroidBridge(string domain, string mode) {
+            AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
+            playerActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+            attentiveAndroidWrapper = new AndroidJavaObject("com.attentive.android.unitywrapper.AttentiveAndroidUnityWrapper");
+            attentiveAndroidWrapper.Call("init", new object[3]{domain, mode, playerActivity});
+        }
+
+        public void Trigger() {
+            attentiveAndroidWrapper.Call("trigger");
+        }
+
+        public void ClearCookies() {
+            attentiveAndroidWrapper.Call("clearCookies");
+        }
+
+#endif
+
+    }
+}
---
+++ Attentive.Unity/Attentive.Unity/AttentiveUnitySDK.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using Attentive.Unity.Android;
+
+
+namespace Attentive.Unity
+{
+    public class AttentiveUnitySDK
+    {
+
+        private AttentiveAndroidBridge androidBridge;
+
+
+        public AttentiveUnitySDK(string domain, string mode)
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+                androidBridge = new AttentiveAndroidBridge(domain, mode);
+#endif
+        }
+
+        public void Trigger()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+                androidBridge.Trigger();
+#endif
+        }
+
+        public void ClearCookies()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+                androidBridge.ClearCookies();
+#endif
+        }
+}
